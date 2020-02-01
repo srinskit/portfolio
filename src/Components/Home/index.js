9fbe2315983912a31483f82a6b568b2b00622972
srinskit <srinskit@users.noreply.github.com>
2020-02-01 14:28:06 +0530

    Quick fix padding

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import {withStyles} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 
-const styles = theme => ({});
+const styles = theme => ({
+    root: {
+        padding: theme.spacing(2),
+    }
+});
 
 class Home extends React.Component {
     render() {
+        const {classes} = this.props;
         return (
-            <div>
+            <div className={classes.root}>
                 <Typography variant={"h1"}>
                     Hi.
                 </Typography>
