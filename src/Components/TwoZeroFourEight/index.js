290738ffda2c14830e09cb70712d0df1637aab0f
srinskit <srinskit@users.noreply.github.com>
2020-04-14 21:04:20 +0530

    Added settings dialog, Added grid size setting

--- src/Components/TwoZeroFourEight/index.js
+++ src/Components/TwoZeroFourEight/index.js
@@ -5,14 +5,34 @@ import {Paper} from "@material-ui/core";
 import Tile from "./Tile";
 import {Swipeable} from "react-swipeable"
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import ClearIcon from '@material-ui/icons/Clear';
+import IconButton from "@material-ui/core/IconButton";
+import SettingsIcon from '@material-ui/icons/Settings';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+
 
 const styles = theme => ({
     root: {
         padding: theme.spacing(0, 2),
     },
-    title: {
-        padding: theme.spacing(4, 0),
+    header: {
+        margin: theme.spacing(4, 0, 2, 0),
+        padding: theme.spacing(0, 1),
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "space-between",
     },
+    title: {},
     grid: {
         touchAction: "none",
         padding: theme.spacing(1),
@@ -23,7 +43,19 @@ const styles = theme => ({
     },
     tileContainer: {
         flexGrow: 1,
-    }
+    },
+    settingsPaper: {
+        margin: theme.spacing(2, 0, 2, 0),
+        padding: theme.spacing(2),
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 80,
+    },
+    gridSizeSelectorWrapper: {
+        display: "flex",
+        alignItems: "center",
+    },
 });
 
 const dirs = ["Up", "Down", "Left", "Right"];
@@ -32,14 +64,17 @@ const dirs = ["Up", "Down", "Left", "Right"];
 class TwoZeroFourEight extends React.Component {
     constructor(props) {
         super(props);
-        const n = 4;
+        const defaultN = 4, defaultM = 4;
         this.swipeConfig = {};
         for (let i = 0; i < dirs.length; ++i) {
             this.swipeConfig[`onSwiped${dirs[i]}`] = this.onMove.bind(this, dirs[i]);
         }
         this.state = {
             stage: "",
-            tiles: initTiles(n),
+            settingsOpen: false,
+            gridN: defaultN,
+            gridM: defaultM,
+            tiles: initTiles(defaultN, defaultM),
         };
         document.addEventListener("keyup", (event) => {
             const keyName = event.key;
@@ -50,32 +85,114 @@ class TwoZeroFourEight extends React.Component {
 
     render() {
         const {classes} = this.props;
-        const {tiles} = this.state;
         return (
-            <Container className={classes.root} maxWidth={"sm"}>
-                <Typography className={classes.title} align={"center"} variant={"h1"}>2048</Typography>
-                <Swipeable {...this.swipeConfig}>
-                    <Paper className={classes.grid} variant={"outlined"}>
-                        {
-                            tiles.map((row, i) => (
-                                    <div key={`${i}`} className={classes.row}>
-                                        {
-                                            row.map((tile, j) =>
-                                                <div className={classes.tileContainer} key={`${i},${j}`}>
-                                                    <Tile val={tile}/>
-                                                </div>
-                                            )
-                                        }
-                                    </div>
-                                )
-                            )
-                        }
-                    </Paper>
-                </Swipeable>
-            </Container>
+            <React.Fragment>
+                <Container className={classes.root} maxWidth={"sm"}>
+                    <div className={classes.header}>
+                        <div><Typography className={classes.title} variant={"h2"}>2048</Typography></div>
+                        <div>
+                            <Grid container>
+                                <Grid item>
+                                    <IconButton>
+                                        <InfoOutlinedIcon fontSize={"large"}/>
+                                    </IconButton>
+                                </Grid>
+                                <Grid item>
+                                    <IconButton onClick={this.onSettingOpen}>
+                                        <SettingsIcon fontSize={"large"}/>
+                                    </IconButton>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </div>
+                    {this.gameView()}
+                </Container>
+                {this.settingsView()}
+            </React.Fragment>
         );
     }
 
+    gameView() {
+        const {classes} = this.props;
+        const {tiles} = this.state;
+        return (
+            <Swipeable {...this.swipeConfig}>
+                <Paper className={classes.grid} variant={"outlined"}>
+                    {
+                        tiles.map((row, i) => (
+                                <div key={`${i}`} className={classes.row}>
+                                    {
+                                        row.map((tile, j) =>
+                                            <div className={classes.tileContainer} key={`${i},${j}`}>
+                                                <Tile val={tile}/>
+                                            </div>
+                                        )
+                                    }
+                                </div>
+                            )
+                        )
+                    }
+                </Paper>
+            </Swipeable>
+        );
+    }
+
+    settingsView() {
+        const {classes} = this.props;
+        const {settingsOpen, gridN, gridM} = this.state;
+        return (
+            <Dialog open={settingsOpen} onClose={this.onSettingClose} maxWidth={"sm"} fullWidth>
+                <DialogTitle>Settings</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Grid size
+                    </DialogContentText>
+                    <div className={classes.gridSizeSelectorWrapper}>
+                        <FormControl variant="outlined" className={classes.formControl}>
+                            <Select
+                                value={gridN}
+                                onChange={this.onChangeGridN}
+                            >
+                                {getGridSizeMenuItems()}
+                            </Select>
+                        </FormControl>
+                        <ClearIcon fontSize={"large"}/>
+                        <FormControl variant="outlined" className={classes.formControl}>
+                            <Select
+                                value={gridM}
+                                onChange={this.onChangeGridM}
+                            >
+                                {getGridSizeMenuItems()}
+                            </Select>
+                        </FormControl>
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant={"outlined"} onClick={this.onSettingClose} color="primary">
+                        Back
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        )
+    }
+
+    onSettingOpen = () => {
+        this.setState({settingsOpen: true});
+    };
+
+    onSettingClose = () => {
+        this.setState({settingsOpen: false});
+    };
+
+    onChangeGridN = (event) => {
+        const gridN = event.target.value;
+        this.setState(prev => ({gridN, tiles: initTiles(gridN, prev.gridM)}));
+    };
+
+    onChangeGridM = (event) => {
+        const gridM = event.target.value;
+        this.setState(prev => ({gridM, tiles: initTiles(prev.gridN, gridM)}));
+    };
 
     handleKeyUp(key) {
         switch (key) {
@@ -91,9 +208,9 @@ class TwoZeroFourEight extends React.Component {
 
     onMove(dir) {
         const {tiles} = this.state;
-        const r = tiles.length, c = tiles.length;
+        const r = tiles.length, c = tiles[0].length;
         let n = dir === "Up" || dir === "Down" ? c : r;
-        const newTiles = makeTiles(tiles.length, tiles.length);
+        const newTiles = makeTiles(r, c);
         for (let i = 0; i < n; ++i) {
             merge(newTiles, tiles, i, dir);
         }
@@ -104,19 +221,25 @@ class TwoZeroFourEight extends React.Component {
             this.setState({tiles: newTiles});
         }
     }
+}
 
-
+function getGridSizeMenuItems() {
+    const items = [];
+    for (let i = 2; i <= 8; ++i) {
+        items.push(<MenuItem key={i} value={i}>{i}</MenuItem>)
+    }
+    return items;
 }
 
 function getRandomTileValue() {
     return 2;
 }
 
-function initTiles(n) {
+function initTiles(n, m) {
     const tiles = [];
     for (let i = 0; i < n; ++i) {
         tiles.push([]);
-        for (let j = 0; j < n; ++j) {
+        for (let j = 0; j < m; ++j) {
             tiles[i].push(randInt(2) ? Math.pow(2, 1 + randInt(3)) : 0);
         }
     }
@@ -138,9 +261,7 @@ function addRandomTile(tiles) {
     const zeros_i = [], zeros_j = [];
     for (let i = 0; i < tiles.length; ++i) {
         for (let j = 0; j < tiles[i].length; ++j) {
-            if (tiles[i][j]) {
-                tiles[i][j] = tiles[i][j];
-            } else {
+            if (!tiles[i][j]) {
                 zeros_i.push(i);
                 zeros_j.push(j);
             }
